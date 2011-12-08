ada04ef460eb2cae254438b5a23455e6bf1ed194
Fabi <fschneider1509@googlemail.com>
2011-12-08 21:31:36 +0100

    Added (very ugly) menubar to the gui. Will make it look nicer tomorrow :).

--- main.c
+++ main.c
@@ -26,14 +26,12 @@ int main (int argc, char *argv[])
 {
 	/*variables*/
 	GtkWidget *main_win;
-	GtkWidget *main_menu_box; /*container box, holds elements*/
-	GtkWidget *main_menubar; /*menu bar*/
-	GtkWidget *main_menubar_file; /*file menu*/
-	GtkWidget *main_menubar_sync; /*sync menu*/
-	GtkWidget *main_menubar_about; /*about menu*/
-	GtkWidget *main_file_open; /*menu entry for file open*/
-	GtkWidget *main_file_exit; /*menu entry for exit application*/
-	GtkWidget *main_sync_sync; /*menu entry for sync folders*/
+	GtkWidget *menu_box; /*container box, holds elements*/
+	GtkWidget *menubar; /*menu bar*/
+	GtkWidget *file_menu; /*file menu*/
+	GtkWidget *file_top; /*top entry*/
+	GtkWidget *open_entry; /*menu entry for file open*/
+	GtkWidget *exit_entry; /*menu entry for exit application*/
 
 
 	gtk_init (&argc, &argv);
@@ -41,28 +39,28 @@ int main (int argc, char *argv[])
 	/*initialize main window*/
 	main_win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	/*initialize menu containerbox*/
-	main_menu_box = gtk_vbox_new( FALSE, 0 );
+	menu_box = gtk_vbox_new( FALSE, 0 );
+	/*add box*/
+	gtk_container_add( GTK_CONTAINER(main_win), menu_box );
+
 	/*initialize menubar*/
-	main_menubar = gtk_menu_bar_new();
+	menubar = gtk_menu_bar_new();
 	/*initialize menus*/
-	main_menubar_file = gtk_menu_new();
-	main_menubar_sync = gtk_menu_new();
-	main_menubar_about = gtk_menu_new();
+	file_menu = gtk_menu_new();
 
 	/*set menu labels*/
-	main_file_open = gtk_menu_item_new_with_label( "Open" );
-	main_file_exit = gtk_menu_item_new_with_label( "Exit" );
-	main_sync_sync = gtk_menu_item_new_with_label( "Sync" );
-
-	/*add box*/
-	gtk_container_add( GTK_CONTAINER(main_win), main_menu_box );
+	file_top = gtk_menu_item_new_with_label( "File" );
+	open_entry = gtk_menu_item_new_with_label( "Open" );
+	exit_entry = gtk_menu_item_new_with_label( "Exit" );
 
 	/*add menus*/
-	gtk_menu_item_set_submenu(GTK_MENU_ITEM(main_file_open), main_menubar_file );
-	gtk_menu_shell_append(GTK_MENU_SHELL(main_menubar_file), main_file_exit );
+	gtk_menu_item_set_submenu( GTK_MENU_ITEM(file_top), file_menu );
+	gtk_menu_shell_append( GTK_MENU_SHELL(file_menu), open_entry );
+	gtk_menu_shell_append( GTK_MENU_SHELL(file_menu), exit_entry );
+	gtk_menu_shell_append( GTK_MENU_SHELL(menubar), file_top );
 
 	/*pack menu*/
-	gtk_box_pack_start( GTK_BOX(main_menu_box), main_menubar, FALSE, FALSE, 3 );
+	gtk_box_pack_start( GTK_BOX(menu_box), menubar, FALSE, FALSE, 3 );
 
 
 	/*set properties*/
