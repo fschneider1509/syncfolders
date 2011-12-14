5467620db28824342d62b9c3b326776c9d07e8a6
Fabian Schneider <fschneider1509@googlemail.com>
2011-12-14 16:37:21 +0100

    Reading folders is enabled in gui (only testpath). Need to add scrollpanes and icons to the treeview.

--- main.c
+++ main.c
@@ -13,52 +13,7 @@
 /*functions*/
 /*main_window*/
 
-/*enum for tree columns*/
-enum
-{
-	NAME_COLUMN, /*folder- or filename*/
-	FSIZE_COLUMN, /*filesize*/
-	CHDATE_COLUMN, /*changedate*/
-	EQUAL_COLUMN, /*equality of the file*/
-	N_COLUMNS /*number of cols*/
-};
-
-void append_files_to_view( GtkTreeStore *pstore, GtkTreeIter *piter, filest *plist, int pnumfiles )
-{
-	/*variables*/
-	int i = 0;
-
-	for( i = 0; i < pnumfiles; i++ )
-	{
-		gtk_tree_store_set( pstore, piter, NAME_COLUMN, plist[i].filename, FSIZE_COLUMN, plist[i].filesize,
-				CHDATE_COLUMN, plist[i].str_changedate, EQUAL_COLUMN, 1, -1 );
-	}
-}
-
-void add_root_to_treeview( folderst *pfolder, GtkTreeStore *pstore )
-{
-	/*variables*/
-	GtkTreeIter additer;
-
-	gtk_tree_store_append( pstore, &additer, NULL );
-
-	/*append files*/
-}
-
-void add_treeview( GtkWidget *playout, GtkWidget **ptview, char ppos )
-{
-	/*variables*/
-	GtkTreeStore *filetree;
-
-
-	filetree = gtk_tree_store_new( N_COLUMNS, G_TYPE_STRING, G_TYPE_INT, G_TYPE_STRING, G_TYPE_INT );
-
-	*ptview = gtk_tree_view_new_with_model( GTK_TREE_MODEL(filetree) );
-
-	gtk_box_pack_end( GTK_BOX(playout), *ptview, FALSE, FALSE, 0 );
-}
-
-void add_menubar( GtkWidget *playout )
+void add_menubar_to_main_win( GtkWidget *playout )
 {
 	/*variables*/
 	GtkWidget *menubar; /*menu bar*/
@@ -113,7 +68,7 @@ void add_menubar( GtkWidget *playout )
 
 }
 
-void set_main_win_attribs( GtkWidget *pwindow, gchar *ptitle )
+void set_main_win_attributes( GtkWidget *pwindow, gchar *ptitle )
 {
 	/*set attributes of the main-window*/
 	gtk_window_set_title (GTK_WINDOW (pwindow), ptitle );
@@ -122,6 +77,103 @@ void set_main_win_attribs( GtkWidget *pwindow, gchar *ptitle )
 	gtk_window_set_default_size( GTK_WINDOW(pwindow), 750, 550 );
 	gtk_window_set_position( GTK_WINDOW(pwindow), GTK_WIN_POS_CENTER );
 }
+
+
+/*enum for tree columns*/
+enum
+{
+	NAME_COLUMN, /*folder- or filename*/
+	FSIZE_COLUMN, /*filesize*/
+	CHDATE_COLUMN, /*changedate*/
+	EQUAL_COLUMN, /*equality of the file*/
+	N_COLUMNS /*number of cols*/
+};
+
+void append_files_to_treeview( GtkTreeStore *pstore, GtkTreeIter *piter, GtkTreeIter *pparent,filest *plist, int pnumfiles )
+{
+	/*variables*/
+	int i = 0;
+
+	for( i = 0; i < pnumfiles; i++ )
+	{
+		/*set current entry of the treeview*/
+		gtk_tree_store_set( pstore, piter,
+				NAME_COLUMN, plist[i].filename,
+				FSIZE_COLUMN, plist[i].filesize,
+				CHDATE_COLUMN, plist[i].str_changedate,
+				EQUAL_COLUMN, "1", -1 );
+
+		/*append remaining files to the list*/
+		if( i < pnumfiles - 1 )
+			gtk_tree_store_append( pstore, piter, pparent );
+	}
+}
+
+void add_folder_to_treeview( folderst *pfolder, GtkTreeStore *pstore, GtkTreeIter *pparent )
+{
+	/*variables*/
+	GtkTreeIter additer;
+	unsigned int i = 0;
+
+	gtk_tree_store_append( pstore, &additer, pparent );
+
+	append_files_to_treeview( pstore, &additer, pparent,(*pfolder).filelist, (*pfolder).numfiles );
+
+	for( i = 0; i < (*pfolder).numfolders; i++ )
+	{
+
+		gtk_tree_store_set( pstore, &additer,
+							NAME_COLUMN, (*pfolder).folderlist[i].foldername,
+							FSIZE_COLUMN, 0,
+							CHDATE_COLUMN, "-",
+							EQUAL_COLUMN, "1", -1 );
+
+		/*append files to folder*/
+		add_folder_to_treeview( &((*pfolder).folderlist[i]), pstore, &additer );
+
+		if( i < (*pfolder).numfolders -1 )
+			gtk_tree_store_append( pstore, &additer, pparent );
+	}
+}
+
+void add_treeview( GtkWidget *playout, GtkWidget **ptview, char ppos )
+{
+	/*variables*/
+	GtkTreeStore *filetree;
+	GtkCellRenderer *treerenderer;
+	GtkTreeViewColumn *namecolumn;
+	GtkTreeViewColumn *sizecolumn;
+	GtkTreeViewColumn *chdatecolumn;
+	GtkTreeViewColumn *equalcolumn;
+	folderst folder_a;
+
+	reset_folder( &folder_a );
+	set_root_folder_attributes( &folder_a, "/home/fabi/temp/folderA" );
+	read_folder( "/home/fabi/temp/folderA", &folder_a );
+
+	filetree = gtk_tree_store_new( N_COLUMNS, G_TYPE_STRING, G_TYPE_INT, G_TYPE_STRING, G_TYPE_STRING );
+
+	add_folder_to_treeview( &folder_a, filetree, NULL );
+
+	*ptview = gtk_tree_view_new_with_model( GTK_TREE_MODEL(filetree) );
+
+	treerenderer = gtk_cell_renderer_text_new();
+	/*define columns*/
+	namecolumn = gtk_tree_view_column_new_with_attributes( "Name", treerenderer, "text", NAME_COLUMN, NULL );
+	sizecolumn = gtk_tree_view_column_new_with_attributes( "Size", treerenderer, "text", FSIZE_COLUMN, NULL );
+	chdatecolumn = gtk_tree_view_column_new_with_attributes( "Changedate", treerenderer, "text", CHDATE_COLUMN, NULL );
+	equalcolumn = gtk_tree_view_column_new_with_attributes( "Equal?", treerenderer, "text", EQUAL_COLUMN, NULL );
+
+	/*append columns*/
+	gtk_tree_view_append_column( GTK_TREE_VIEW(*ptview), namecolumn );
+	gtk_tree_view_append_column( GTK_TREE_VIEW(*ptview), sizecolumn );
+	gtk_tree_view_append_column( GTK_TREE_VIEW(*ptview), chdatecolumn );
+	gtk_tree_view_append_column( GTK_TREE_VIEW(*ptview), equalcolumn );
+
+	gtk_box_pack_start( GTK_BOX(playout), *ptview, FALSE, FALSE, 0 );
+}
+
+
 
 void start_gtk_gui( void )
 {
@@ -140,10 +192,10 @@ void start_gtk_gui( void )
 	gtk_container_add( GTK_CONTAINER(main_win), main_layout );
 
 	/*set properties*/
-	set_main_win_attribs( main_win, "syncfolders-gtk" );
+	set_main_win_attributes( main_win, "syncfolders-gtk" );
 
 	/*add menubar*/
-	add_menubar( main_layout );
+	add_menubar_to_main_win( main_layout );
 
 	/*add treeview*/
 	add_treeview( main_layout, &main_tree_a, 'l' );
